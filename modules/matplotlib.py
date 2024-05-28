ffeb48344ff825649e449f29143322b4a6638375
Erik Buis <ebbuis@gmail.com>
2024-05-28 14:05:14 +0200

    Added Arrow3D class to draw 3D arrows in 3D plots

--- modules/matplotlib.py
+++ modules/matplotlib.py
@@ -1,4 +1,4 @@
-from typing import Literal, Sequence
+from typing import Any, Literal, Sequence
 
 import matplotlib
 import matplotlib.axes
@@ -7,6 +7,8 @@ import matplotlib.colors
 import matplotlib.figure
 import numpy as np
 import numpy.typing as npt
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import Axes3D, proj3d
 
 
 def get_scalar_mappable(
@@ -324,3 +326,104 @@ def plot_polyfit(
     ax.fill_between(x_poly, y_lower, y_upper, **kwargs_confidence)
 
     return coefficients
+
+
+class Arrow3D(FancyArrowPatch):
+    """A 3D arrow that can be plotted on an Axes3D instance.
+
+    Taken from:
+    https://stackoverflow.com/questions/22867620/
+    putting-arrowheads-on-vectors-in-matplotlibs-3d-plot
+
+    This class draws an arrow using an ArrowStyle. To draw an arrow, you should
+    call arrow3D() instead of creating an instance of this class directly.
+
+    The head and tail positions are fixed at the specified start and end points
+    of the arrow, but the size and shape (in display coordinates) of the arrow
+    does not change when the axis is moved or zoomed.
+    """
+
+    def __init__(
+        self, x, y, z, dx, dy, dz, *args, **kwargs  # type: ignore
+    ) -> None:
+        super().__init__((0, 0), (0, 0), *args, **kwargs)
+        self._xyz = (x, y, z)
+        self._dxdydz = (dx, dy, dz)
+
+    def do_3d_projection(self, renderer=None) -> float:  # type: ignore
+        x1, y1, z1 = self._xyz
+        dx, dy, dz = self._dxdydz
+        x2, y2, z2 = (x1 + dx, y1 + dy, z1 + dz)
+
+        xs, ys, zs = proj3d.proj_transform(
+            (x1, x2), (y1, y2), (z1, z2), self.axes.M  # type: ignore
+        )
+        self.set_positions((xs[0], ys[0]), (xs[1], ys[1]))
+
+        return np.min(zs)
+
+
+def arrow3D(
+    ax: Axes3D,
+    x: float | npt.ArrayLike,
+    y: float | npt.ArrayLike,
+    z: float | npt.ArrayLike,
+    dx: float | npt.ArrayLike,
+    dy: float | npt.ArrayLike,
+    dz: float | npt.ArrayLike,
+    *args: Any,
+    **kwargs: Any,
+) -> None:
+    """Add an 3D arrow to an Axes3D instance.
+
+    Note:
+        In contrast to other patches, the default capstyle and joinstyle for
+        Arrow3D are set to "round".
+
+    Args:
+        ax: The Axes3D instance to add the arrow to.
+        x: The x-coordinate of the tail of the arrow.
+        y: The y-coordinate of the tail of the arrow.
+        z: The z-coordinate of the tail of the arrow.
+        dx: The difference in x-coordinates between the head and tail of the
+            arrow.
+        dy: The difference in y-coordinates between the head and tail of the
+            arrow.
+        dz: The difference in z-coordinates between the head and tail of the
+            arrow.
+        *args: Additional arguments to pass to the
+            matplotlib.patches.FancyArrowPatch constructor.
+        **kwargs: Additional keyword arguments to pass to the
+            matplotlib.patches.FancyArrowPatch constructor. Here is a list of
+            the available FancyArrowPatch properties:
+
+        arrowstyle (str or matplotlib.patches.ArrowStyle, default: "simple"):
+            The ArrowStyle with which the fancy arrow is drawn. If a string,
+            it should be one of the available arrowstyle names, with optional
+            comma-separated attributes. The optional attributes are meant to
+            be scaled with the mutation_scale. The available names can be found
+            at matplotlib.patches.ArrowStyle.
+        connectionstyle (str or matplotlib.patches.ConnectionStyle or None,
+            default: "arc3"): The ConnectionStyle with which the tail and head
+            are connected. If a string, it should be one of the available
+            connectionstyle names, with optional comma-separated attributes.
+            The available names can be found at
+            matplotlib.patches.ConnectionStyle.
+        patchA (matplotlib.patches.Patch, default: None): The patch for the
+            head of the arrow.
+        patchB (matplotlib.patches.Patch, default: None): The patch for the
+            tail of the arrow.
+        shrinkA (float, default: 2): Shrinking factor of the tail of the arrow.
+        shrinkB (float, default: 2): Shrinking factor of the head of the arrow.
+        mutation_scale (float, default: 1): Value with which attributes of
+            arrowstyle (e.g. head_length) will be scaled.
+        mutation_aspect (None or float, default: None): The height of the
+            rectangle will be squeezed by this value before the mutation and
+            the mutated box will be stretched by the inverse of it.
+        **kwargs: Additional keyword arguments to pass to the
+            matplotlib.patches.Patch constructor. A list of the available Patch
+            properties can be found at:
+            https://matplotlib.org/stable/api/_as_gen/matplotlib.patches.Patch
+    """
+    arrow = Arrow3D(x, y, z, dx, dy, dz, *args, **kwargs)
+    ax.add_artist(arrow)
