7c97b4cc1f1c1e63e09d087f000938e803db05c0
Erik Buis <ebbuis@gmail.com>
2024-11-11 16:57:45 +0100

    Undid last change because using nans as padding would convert the tensor to a float tensor, which is not the expected behavior.

--- python_utils/custom_batched/geometry.py
+++ python_utils/custom_batched/geometry.py
@@ -584,11 +584,11 @@ def xiaolin_wu_anti_aliasing_batched(
 
     Returns:
         Tuple containing:
-        - Pixel x-coordinates, padded with NaN values.
+        - Pixel x-coordinates, padded with zeros.
             Shape: [B, max(S_b)]
-        - Pixel y-coordinates, padded with NaN values.
+        - Pixel y-coordinates, padded with zeros.
             Shape: [B, max(S_b)]
-        - Pixel values between 0 and 1, padded with NaN values.
+        - Pixel values between 0 and 1, padded with zeros.
             Shape: [B, max(S_b)]
         - The number of pixels in each line segment.
             Shape: [B]
@@ -650,12 +650,8 @@ def xiaolin_wu_anti_aliasing_batched(
     vals[torch.arange(B), S_bs - 1] *= xgap_end
 
     # Pad the return values.
-    replace_padding_batched(
-        pixels_x, S_bs, padding_value=torch.nan, in_place=True
-    )
-    replace_padding_batched(
-        pixels_y, S_bs, padding_value=torch.nan, in_place=True
-    )
-    replace_padding_batched(vals, S_bs, padding_value=torch.nan, in_place=True)
+    replace_padding_batched(pixels_x, S_bs, in_place=True)
+    replace_padding_batched(pixels_y, S_bs, in_place=True)
+    replace_padding_batched(vals, S_bs, in_place=True)
 
     return pixels_x, pixels_y, vals, S_bs
