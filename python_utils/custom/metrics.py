882d799a897dfb7bc978adf890ffd267ee2fe32d
Erik Buis <ebbuis@gmail.com>
2024-11-15 20:43:32 +0100

    Fixed bug where logging levels below INFO would not be applied

--- python_utils/custom/metrics.py
+++ python_utils/custom/metrics.py
@@ -752,7 +752,7 @@ if __name__ == "__main__":
         ),
         filter={
             "": "INFO",  # Default level for external libraries.
-            __name__: "TRACE",  # All levels for the main file.
+            "__main__": "TRACE",  # All levels for the main file.
             __package__: "TRACE",  # All levels for internal modules.
         },
     )
