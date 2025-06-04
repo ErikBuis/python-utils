8f0fbf732c45a2a0659e985b136449d8bc562f76
Erik Buis <ebbuis@gmail.com>
2025-06-04 20:25:04 +0200

    Added comparison of pytorch's unique() to this repo's custom unique function

--- python_utils/custom/plot_times/unique_torch/unique_torch.py
+++ python_utils/custom/plot_times/unique_torch/unique_torch.py
@@ -6,43 +6,35 @@ import random
 from typing import Any
 
 import numpy as np
-import numpy.typing as npt
+import torch
 
-from python_utils.modules.numpy import unique
+from python_utils.modules.torch import unique
 
 from ..plot_times import plot_times
 
 
 def approach_1(
-    x: npt.NDArray, axis: int
-) -> tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray]:
-    """Approach 1: Use NumPy's built-in unique function."""
-    return np.unique(
-        x, return_index=True, return_inverse=True, return_counts=True, axis=axis
-    )
+    x: torch.Tensor, dim: int
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+    """Approach 1: Use PyTorch's built-in unique function."""
+    return torch.unique(x, return_inverse=True, return_counts=True, dim=dim)
 
 
 def approach_2(
-    x: npt.NDArray, axis: int
-) -> tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray]:
+    x: torch.Tensor, dim: int
+) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     """Approach 2: Use custom unique function."""
-    return unique(
-        x,
-        return_backmap=True,
-        return_inverse=True,
-        return_counts=True,
-        axis=axis,
-    )
+    return unique(x, return_inverse=True, return_counts=True, dim=dim)
 
 
 def map_to_inputs(
     amount_rows: int, amount_cols: int
-) -> tuple[tuple[npt.NDArray], dict[str, Any]]:
+) -> tuple[tuple[torch.Tensor], dict[str, Any]]:
     """Map input parameters to function arguments.
 
     Args:
-        amount_rows: The number of rows in the array.
-        amount_cols: The number of columns in the array.
+        amount_rows: The number of rows in the tensor.
+        amount_cols: The number of columns in the tensor.
 
     Returns:
         Tuple containing positional args and keyword args for the algorithms.
@@ -50,11 +42,11 @@ def map_to_inputs(
     # Set random seed for reproducibility.
     random.seed(69)
 
-    # Generate a random array with the specified dimensions.
-    x = np.random.randint(0, 10, size=(amount_rows, amount_cols))
+    # Generate a random tensor with the specified dimensions.
+    x = torch.randint(0, 10, size=(amount_rows, amount_cols))
 
     # Return the inputs.
-    return (x,), {"axis": 0}
+    return (x,), {"dim": 0}
 
 
 def main(args: argparse.Namespace) -> None:
@@ -72,8 +64,8 @@ def main(args: argparse.Namespace) -> None:
         map_to_inputs,
         approach_1,
         approach_2,
-        "NumPy unique comparison",
-        "NumPy's np.unique",
+        "PyTorch unique comparison",
+        "PyTorch's torch.unique",
         "Custom unique function",
         "Amount of rows",
         "Amount of columns",
